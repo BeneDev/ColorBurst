9911423f09dff37910f0c4c6e479bc50a3aa95d2
BeneDev <33699887+BeneDev@users.noreply.github.com>
2018-01-27 11:06:02 +0100

    SetOutline
    You can set the outline now when you have the hide object

--- Assets/Scripts/HideObjController.cs
+++ Assets/Scripts/HideObjController.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class HideObjController : MonoBehaviour {
 
     public bool shineOn = false;
+    Material mat;
 
 	// Use this for initialization
 	void Start () {
-		
+        mat = GetComponent<Renderer>().material;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
+
+    public void SetOutline(float thickness)
+    {
+        mat.SetFloat("_OutlineWidth", thickness);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
